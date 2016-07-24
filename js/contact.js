6421d0616f42120377f4b9874f45afb031afba5e
Sanya V <avHITlearn@gmail.com>
2016-07-24 21:09:24 +0300

    Added Email sender.

--- js/contact.js
+++ js/contact.js
@@ -73,7 +73,7 @@
                 }
             }
         }).on('success.form.bv', function (e) {
-            $('#success_message').slideDown({opacity: "show"}, "slow") // Do something ...
+            $('#success_message').slideDown({opacity: "show"}, "slow");// Do something ...
             $('#contact_form').data('bootstrapValidator').resetForm();
 
             // Prevent form submission
@@ -86,10 +86,29 @@
             var bv = $form.data('bootstrapValidator');
 
             // Use Ajax to submit form data
-            $.post($form.attr('action'), $form.serialize(), function (result) {
+            $.get($form.attr('action'), $form.serialize(), function (result) {
                 console.log(result);
             }, 'json');
         });
+    });
+
+    $(document).on('submit','.myForm',function(){
+        emailjs.send("default_service","my_tamplate",{
+            firstName: $('#contactFirstName').val(),
+            lastName: $('#contactLastName').val(),
+            from:$('#contactEmail').val(),
+            subject:$('#contactSubject').val(),
+            content:$('#contactComment').val(),
+            phone:$('#contactPhone').val()
+        })
+            .then(
+                function(response) {
+                    console.log("SUCCESS", response);
+                },
+                function(error) {
+                    console.log("FAILED", error);
+                }
+            );
     });
 }(jQuery));
 
